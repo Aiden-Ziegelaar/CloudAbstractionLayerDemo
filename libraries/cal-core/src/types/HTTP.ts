9bc75549ad441d30778fac51078b20da95dc6560
Aiden Ziegelaar <aiden.ziegelaar@energyaustralia.com.au>
2023-02-03 18:47:50 +0800

    added CAL libs

--- libraries/cal-core/src/types/HTTP.ts
+++ libraries/cal-core/src/types/HTTP.ts
@@ -1,4 +1,14 @@
-export abstract class HTTPIntegration {
+export enum HTTPMethods {
+  'OPTIONS',
+  
+}
 
-
+export abstract class HTTPIntegrationRequest {
+  public baseUrl!: string;
+  public body!: object;
+  public cookies!: Record<string, string|string[]>;
+  public hostname!: string;
+  public ip!: string;
+  public ips!: string;
+  public method!:
 }
